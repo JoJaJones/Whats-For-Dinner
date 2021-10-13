7ee594df8e0cbf2d037ccecfa631867e138149a1
Josh Jones <jonesjo7@oregonstate.edu>
2021-10-13 12:32:35 -0700

    init commit on branch

---
+++ lib/controllers/PantryManager.dart
@@ -0,0 +1,8 @@
+class PantryManager {
+  static const PANTY_COLLECTION = "PantryItems";
+  static const NAME_KEY = "name";
+  static const QUANTITY_KEY = "quantity";
+  static const EXPIRATION_KEY = "expiration";
+
+  List<PantryItem> pantry;
+}
