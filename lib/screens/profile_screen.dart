0a02a028dbcbfe747f4dff4aa687ec73727f342b
GavinSlusher <GavinSlusher@gmail.com>
2021-10-30 11:03:25 -0500

    Update Profile screen card UI

--- lib/screens/profile_screen.dart
+++ lib/screens/profile_screen.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:whats_for_dinner/controllers/UserController.dart';
 import 'package:whats_for_dinner/widgets/ProfileWidget.dart';
@@ -16,27 +17,55 @@ class ProfileScreen extends StatelessWidget {
           ),
           SliverGrid(
             gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
-              maxCrossAxisExtent: 100.0,
+              maxCrossAxisExtent: 150.0,
             ),
             delegate: SliverChildBuilderDelegate(
               (BuildContext context, int index) {
                 return Card(
-                    elevation: 10,
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(20.0),
-                    ),
-                    child: SizedBox(
-                      width: 100.0,
-                      height: 100.0,
-                      child: Container(
-                        alignment: Alignment.center,
-                        color: Colors.white,
-                        child: Text(
-                          'Favorite $index',
-                          style: TextStyle(color: Colors.grey),
+                  elevation: 10,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(20.0),
+                  ),
+                  child: Column(
+                    children: [
+                      Expanded(
+                        flex: 1,
+                        child: ListTile(
+                          contentPadding: EdgeInsets.symmetric(horizontal: 10),
+                          horizontalTitleGap: 5,
+                          trailing: IconButton(
+                            padding: const EdgeInsets.all(4),
+                            splashRadius: 15,
+                            iconSize: 15,
+                            // alignment: Alignment.topRight,
+                            icon: Icon(
+                              Icons.favorite,
+                              color: Colors.redAccent,
+                            ),
+                            onPressed: () {
+                              // TODO remove recipe from favorite
+                            },
+                          ),
+                          title: Text(
+                            'Recipe Title',
+                            maxLines: 2,
+                            textAlign: TextAlign.center,
+                            style: TextStyle(fontSize: 13),
+                          ),
                         ),
                       ),
-                    ));
+                      Expanded(
+                        flex: 2,
+                        child: ClipOval(
+                          clipBehavior: Clip.antiAlias,
+                          child: Image.network(
+                            'https://images.unsplash.com/photo-1567620905732-2d1ec7ab7445?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=780&q=80',
+                          ),
+                        ),
+                      )
+                    ],
+                  ),
+                );
               },
               childCount: 20,
             ),
