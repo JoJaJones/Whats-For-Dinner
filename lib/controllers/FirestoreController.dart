2f1337f9897efdd40716789a70461a0bbf358d7d
Josh Jones <jonesjo7@oregonstate.edu>
2021-11-10 22:42:14 -0800

    Added some userId funct

--- lib/controllers/FirestoreController.dart
+++ lib/controllers/FirestoreController.dart
@@ -1,11 +1,13 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_storage/firebase_storage.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'dart:io';
 
 class FirestoreController {
   static final FirestoreController _fsController = FirestoreController._internal();
 
   static const String IMAGE_URL = "image_url";
+  static String? userId;
 
   FirebaseStorage _fileStorage;
   FirebaseFirestore _db;
@@ -16,9 +18,12 @@ class FirestoreController {
 
   FirestoreController._internal()
       : _db = FirebaseFirestore.instance,
-        _fileStorage = FirebaseStorage.instance {}
+        _fileStorage = FirebaseStorage.instance {
+    _initUser();
+  }
 
   Future<void> uploadFile(File image, Map<String, dynamic> refData) async {
+    _initUser();
     var storageRef =
     _fileStorage.ref().child('${Timestamp.now().hashCode}.jpg');
     UploadTask uploadTask = storageRef.putFile(image);
@@ -26,19 +31,73 @@ class FirestoreController {
     refData[IMAGE_URL] = await storageRef.getDownloadURL();
   }
 
-  Future<void> addEntryToDatabase(String collection, Map<String, dynamic> data) async {
+  Future<void> addEntryToCollection(String collection, Map<String, dynamic> data) async {
+    _initUser();
     _db.collection(collection).add(
         data
     );
   }
 
+  Future<void> addEntryToUserCollection(String collection, Map<String, dynamic> data) async {
+    _initUser();
+    if(userId != null) {
+      _db.collection("$userId-$collection").add(
+          data
+      );
+    }
+  }
+
+  Future<void> addEntryToDoc(String collection, String document, Map<String, dynamic> data) async {
+    _initUser();
+    _db.collection(collection).doc(document).add(
+        data
+    );
+  }
+
+  Future<void> addEntryToUserDoc(String collection, String document, Map<String, dynamic> data) async {
+    _initUser();
+    if(userId != null) {
+      _db.collection("$userId-$collection").doc(document).add(
+          data
+      );
+    }
+  }
+
   Stream readDatabaseEntryList(String collection){
+    _initUser();
     return _db.collection(collection).snapshots();
   }
 
-  Stream readDatabaseEntry(String collection, String doc){
-    return _db.collection(collection).doc(doc).snapshots();
+  Stream readDatabaseEntry(String collection, String document){
+    _initUser();
+    return _db.collection(collection).doc(document).snapshots();
   }
 
+
+  Stream readUserDatabaseEntryList(String collection){
+    _initUser();
+    if(userId != null) {
+      return _db.collection("$userId-$collection").snapshots();
+    }
+
+    return Stream.empty();
+  }
+
+  Stream readUserDatabaseEntry(String collection, String document){
+    _initUser();
+    if(userId != null) {
+      return _db.collection("$userId-$collection").doc(document).snapshots();
+    }
+
+    return Stream.empty();
+  }
+
+  void _initUser(){
+    if(userId == null){
+      userId = FirebaseAuth.instance.currentUser?.uid;
+    }
+  }
+
+  String? get userid => userId;
 
 }
