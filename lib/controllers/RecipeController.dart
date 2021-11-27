b96124915d27dcd1a6dcc6173fad8c14c642bd52
Peyton Fitzgerald <pdf272@gmail.com>
2021-11-27 16:26:41 -0500

    basic functionality to only show recipes w/ corresponding ingredients in the pantry, also adding fuzzywuzzy for better search capabilities

--- lib/controllers/RecipeController.dart
+++ lib/controllers/RecipeController.dart
@@ -4,6 +4,7 @@ import 'package:whats_for_dinner/controllers/PantryManager.dart';
 import 'package:whats_for_dinner/controllers/FirestoreController.dart';
 import '../models/Recipe.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:fuzzywuzzy/fuzzywuzzy.dart';
 
 class RecipeController {
   Map filters = {
@@ -17,11 +18,13 @@ class RecipeController {
   static var pantry = PantryManager();
   static var firestore = FirestoreController();
   static List<Recipe> allRecipes = [];
+  static List<Recipe> pantryRecipes = [];
 
   /// **************************************************************************
   /// Function to load all recipes currently in firebase
   /// **************************************************************************
   static void loadRecipes() {
+    print("loading recipes");
     List<Recipe> firestoreRecipes = [];
     var recipes = firestore.readDatabaseEntryList('recipes');
     recipes.listen((snapshot) {
@@ -30,8 +33,33 @@ class RecipeController {
         Recipe curRecipe = Recipe.fromJson(doc.data() as Map<String, dynamic>);
         firestoreRecipes.add(curRecipe);
       });
+      pantryRecipes = determinePantryRecipes(firestoreRecipes);
     });
     allRecipes = firestoreRecipes;
+  }
+
+  static List<Recipe> determinePantryRecipes(recipeList) {
+    List<Recipe> pantryList = [];
+    List pantryIngredients = pantry.ingredientString.split(',');
+    for (Recipe recipe in recipeList) {
+      bool notFound = true;
+      for (var recipeIngredient in recipe.extendedIngredients) {
+        String curRecipeIngredient =
+            (recipeIngredient["name"]?.toLowerCase() ?? "").toLowerCase();
+        if (notFound) {
+          for (String pantryIngredient in pantryIngredients) {
+            String curPantryIngredient = pantryIngredient.toLowerCase();
+            if (ratio(curRecipeIngredient, curPantryIngredient) >= 75) {
+              pantryList.add(recipe);
+              notFound = false;
+            }
+          }
+        } else {
+          break;
+        }
+      }
+    }
+    return pantryList;
   }
 
   /// **************************************************************************
@@ -44,9 +72,11 @@ class RecipeController {
         "there are ${pantry.ingredientCounts} ingredients: ${pantry.ingredientString}");
     if (pantry.ingredientString.length != 0) {
       print("Loading ingredients");
+      print(pantry.ingredientString);
       List ingredientList = pantry.ingredientString.split(',');
       // grab recipe IDs for recipes retrieved from spoonacular that aren't
       // already stored in our database
+
       newRecipeIDs = await RecipesApi.getRecipeIDs((ingredientList));
       // if new recipe IDs were found, fetch their info, and add to firebase
       if (newRecipeIDs.length != 0) {
@@ -60,12 +90,21 @@ class RecipeController {
   }
 
   /// *******************************************************************
-  /// Returns all relevant recipes for current pantry
+  /// Returns all recipes from database
   ///*******************************************************************/
   static Future<List<Recipe>> getAllRecipes() async {
     updateFirestore();
     loadRecipes();
     return allRecipes;
+  }
+
+  /// *******************************************************************
+  /// Returns all relevant recipes for current pantry
+  ///*******************************************************************/
+  static Future<List<Recipe>> getAllPantryRecipes() async {
+    updateFirestore();
+    loadRecipes();
+    return pantryRecipes;
   }
 
   /// *******************************************************************
@@ -79,7 +118,17 @@ class RecipeController {
   /// Searches the current list of Recipe objects for those that match a
   /// given search query
   ///*******************************************************************/
-  static Future<List<Recipe>> searchRecipes(String query) async {
+  static Future<List<Recipe>> searchRecipes(String query,
+      {pantry = false}) async {
+    if (pantry) {
+      return pantryRecipes.where((recipe) {
+        final titleLower = recipe.title.toLowerCase();
+        final summaryLower = recipe.summary.toLowerCase();
+        final searchLower = query.toLowerCase();
+        return titleLower.contains(searchLower) ||
+            summaryLower.contains(searchLower) && meetsFilterCriteria(recipe);
+      }).toList();
+    }
     return allRecipes.where((recipe) {
       final titleLower = recipe.title.toLowerCase();
       final summaryLower = recipe.summary.toLowerCase();
